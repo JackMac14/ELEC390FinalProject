4df180eecb79fc9c2c223123032a2b89c2487ba9
JackMac14 <114034303+JackMac14@users.noreply.github.com>
2023-04-11 15:34:57 -0400

    Update Step2_Storing.py

--- Step2_Storing.py
+++ Step2_Storing.py
@@ -2,14 +2,12 @@ import h5py
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-
 def segment_data(data, window_size, overlap):
     segments = []
     for i in range(0, len(data) - window_size, overlap):
         segment = data[i:i + window_size]
         segments.append(segment)
     return segments
-
 
 # Read in the CSV files for each person
 kieran_walking = pd.read_csv('Accelerometer_Walking_Kieran.csv')
@@ -31,30 +29,55 @@ amir_jumping_segments = segment_data(amir_jumping.values, window_size, overlap)
 jack_walking_segments = segment_data(jack_walking.values, window_size, overlap)
 jack_jumping_segments = segment_data(jack_jumping.values, window_size, overlap)
 
-# Create the HDF5 file and base group
+# Create the HDF5 file
 f = h5py.File('data.h5', 'w')
 
-# Create groups for each person and add segmented walking and jumping datasets
-for name, walking_segments, jumping_segments in [('Kieran', kieran_walking_segments, kieran_jumping_segments),
-                                                 ('Amir', amir_walking_segments, amir_jumping_segments),
-                                                 ('Jack', jack_walking_segments, jack_jumping_segments)]:
-    person_group = f.create_group(name)
-    person_group.create_dataset('walking', data=walking_segments)
-    person_group.create_dataset('jumping', data=jumping_segments)
+# Create group for each person
+jack_group = f.create_group('Jack')
+kieran_group = f.create_group('Kieran')
+amir_group = f.create_group('Amir')
 
-    # Shuffle and split walking and jumping data into train and test sets
-    walking_train, walking_test = train_test_split(walking_segments, test_size=0.1, random_state=42)
-    jumping_train, jumping_test = train_test_split(jumping_segments, test_size=0.1, random_state=42)
+# Create subgroups for jumping and walking data under each person's group
+jack_jumping_group = jack_group.create_group('jumping')
+jack_walking_group = jack_group.create_group('walking')
+kieran_jumping_group = kieran_group.create_group('jumping')
+kieran_walking_group = kieran_group.create_group('walking')
+amir_jumping_group = amir_group.create_group('jumping')
+amir_walking_group = amir_group.create_group('walking')
 
-    # Create 'Train' group and add walking and jumping train datasets
-    train_group = f.create_group(f'{name}/Train')
-    train_group.create_dataset('walking', data=walking_train)
-    train_group.create_dataset('jumping', data=jumping_train)
+# Store jumping and walking segments under each person's group
+jack_jumping_group.create_dataset('data', data=jack_jumping_segments)
+jack_walking_group.create_dataset('data', data=jack_walking_segments)
+kieran_jumping_group.create_dataset('data', data=kieran_jumping_segments)
+kieran_walking_group.create_dataset('data', data=kieran_walking_segments)
+amir_jumping_group.create_dataset('data', data=amir_jumping_segments)
+amir_walking_group.create_dataset('data', data=amir_walking_segments)
 
-    # Create 'Test' group and add walking and jumping test datasets
-    test_group = f.create_group(f'{name}/Test')
-    test_group.create_dataset('walking', data=walking_test)
-    test_group.create_dataset('jumping', data=jumping_test)
+# Create dataset group
+dataset_group = f.create_group('dataset')
+
+# Shuffle and store jumping and walking segments in Training and Testing groups under dataset group
+jumping_segments = kieran_jumping_segments + amir_jumping_segments + jack_jumping_segments
+walking_segments = kieran_walking_segments + amir_walking_segments + jack_walking_segments
+
+shuffled_jumping_segments = train_test_split(jumping_segments, test_size=0.1, random_state=42)
+shuffled_walking_segments = train_test_split(walking_segments, test_size=0.1, random_state=42)
+
+
+
+# Store shuffled jumping and walking segments in Training group
+training_group = dataset_group.create_group('Train')
+training_jumping_group = training_group.create_group('jumping')
+training_jumping_group.create_dataset('data', data=shuffled_jumping_segments[0])
+training_walking_group = training_group.create_group('walking')
+training_walking_group.create_dataset('data', data=shuffled_walking_segments[0])
+
+# Store shuffled jumping and walking segments in Testing group
+testing_group = dataset_group.create_group('Test')
+testing_jumping_group = testing_group.create_group('jumping')
+testing_jumping_group.create_dataset('data', data=shuffled_jumping_segments[1])
+testing_walking_group = testing_group.create_group('walking')
+testing_walking_group.create_dataset('data', data=shuffled_walking_segments[1])
 
 # Close the HDF5 file
 f.close()
